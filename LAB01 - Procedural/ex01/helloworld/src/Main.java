49efb079a8c1286ec88d1fc8a2835ac3d78707a0
marcella314 <marcella.figueredo@ufu.br>
2023-03-16 09:35:10 -0300

    soluçao ex02

--- LAB01 - Procedural/ex01/helloworld/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Marcella Silva Figueredo");
-        System.out.println("12121BCC029");
-    }
-}
